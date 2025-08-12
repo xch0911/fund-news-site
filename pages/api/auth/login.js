aa5ae3a949d8edacb21dca97099e0b160e0483c6
xuchangheng <xuchangheng@goldenalpha.com.cn>
2025-08-12 15:38:38 +0800

    generate

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -8,7 +8,8 @@ export default async function handler(req,res){
   const user = await prisma.user.findUnique({ where: { username } })
   if(!user) return res.status(401).json({ error: '用户名或密码错误' })
   const ok = await bcrypt.compare(password, user.passwordHash);
-  console.log(ok);
+  const hash = await bcrypt.hash(password, 10)
+  console.log(hash);
   if(!ok) return res.status(401).json({ error: '用户名或密码错误' })
   const token = signToken({ id: user.id, username: user.username, role: user.role })
   setTokenCookie(res, token)
